d458f02c134c78faa0f915835da07259ea30763c
Chinmay Purav <digitalnss7@gmail.com>
2021-02-05 00:06:45 +0530

    Added Bank Model with migrations

---
+++ database/migrations/2021_02_04_183459_create_banks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('bank_code')->unique();
+            $table->string('manager_email');
+            $table->string('address');
+            $table->string('postal');
+            $table->decimal('lat', 8, 5);
+            $table->decimal('lon', 8, 5);
+            $table->unsignedTinyInteger('status_code')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
