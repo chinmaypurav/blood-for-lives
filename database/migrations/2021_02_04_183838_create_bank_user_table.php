e9441ea82128820e05fef63919e7c0de79b587d9
Chinmay Purav <digitalnss7@gmail.com>
2021-02-05 00:09:46 +0530

    Added Bank User Migration

---
+++ database/migrations/2021_02_04_183838_create_bank_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_user', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                    ->constrained();
+            $table->foreignId('bank_id')
+                    ->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_user');
+    }
+}
