3d436698147e67330f62fff8704cb18e9d162aa0
Chinmay Purav <digitalnss7@gmail.com>
2021-02-07 20:01:03 +0530

    Donation Transaction Migration added

---
+++ database/migrations/2021_02_07_142720_create_bank_donor_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankDonorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_donor', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('donor_id')
+                    ->constrained();
+            $table->foreignId('bank_id')
+                    ->constrained();
+            $table->enum('blood_component', ['whole', 'plasma', 'platelets', 'wbc', 'rbc']);
+            $table->string('editor');
+            $table->timestamp('donated_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_donor');
+    }
+}
