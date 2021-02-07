f72f83df4cd037de876a65c50e8a9438817bf637
Chinmay Purav <digitalnss7@gmail.com>
2021-02-07 19:16:37 +0530

    Added DonorController with Request

---
+++ app/Http/Controllers/Manager/DonorController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Manager\DonorRequest;
+
+class DonorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('manager.donor.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('manager.donor.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DonorRequest $request)
+    {
+        $validated = $request->validated();
+        //dd($validated);
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->donor()->create([
+            'blood_group'   => $validated['bloodGroup'],
+            'contact'       => $validated['contact'],
+            'postal'        => $validated['postal'],
+            'dob'           => $validated['dob'],
+            'donor_card_no' => 0, //Temporary set to zero.
+            'lat'           => 0, //Temporary set to zero.
+            'lon'           => 0, //Temporary set to zero.
+        ]);
+        return redirect()->route('manager.donor.create')->with('status', 'Donor Added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Search 
+     *
+     *
+     */
+    public function search(Request $request)
+    {
+        return "Search Ok";
+    }
+}
