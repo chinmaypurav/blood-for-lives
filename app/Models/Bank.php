48428c1227f7bde49fa9a03c8716820dd5660e49
Chinmay Purav <digitalnss7@gmail.com>
2021-02-10 11:27:38 +0530

    Added Donors Method

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -23,4 +23,11 @@ class Bank extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    public function donors()
+    {
+        return $this->belongsToMany(Donor::class)
+        ->withPivot('id', 'donated_at', 'editor', 'blood_component', 'status_code')
+        ->orderByDesc('donated_at');
+    }
 }
