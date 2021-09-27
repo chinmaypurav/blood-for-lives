88fb6a29a01e6c2d7a278e781b2872101c3d8d88
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2021-09-28 00:11:07 +0530

    wip

--- database/factories/BankFactory.php
+++ database/factories/BankFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Bank;
 use App\Models\Camp;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BankFactory extends Factory
@@ -25,6 +26,8 @@ class BankFactory extends Factory
         return $this->afterMaking(function (Bank $bank) {
             //
         })->afterCreating(function (Bank $bank) {
+            User::factory()->for($bank)->managerAdmin()->create();
+            User::factory(mt_rand(0,5))->for($bank)->manager()->create();
             Camp::factory(mt_rand(0,5))->for($bank)->create();
         });
     }
