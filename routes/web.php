b03480864cda6dabc12fb3169e2542f1372f73fe
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2021-10-03 12:45:33 +0530

    wip

--- routes/web.php
+++ routes/web.php
@@ -37,78 +37,66 @@ Route::get('bank/register/{bank:bank_code}', [App\Http\Controllers\Bank\BankRegi
 Route::post('bank/register/{bank:bank_code}', [App\Http\Controllers\Bank\BankRegisterController::class, 'store'])
     ->middleware('guest');
 
-// Route::view('bank/register/{bank}', 'bank.register')->middleware('signed')->name('bank.register');
-
 Route::group([
-    'prefix' => 'bank',
-    'as' => 'bank.',
     'middleware' => 'auth'
 ], function () {
-    Route::get('ada/{id}', [App\Http\Controllers\Bank\AdaController::class, 'create'])->name('ada.create');
-    Route::post('ada', [App\Http\Controllers\Bank\AdaController::class, 'store'])->name('ada.store');
-    Route::get('batch', [App\Http\Controllers\Bank\AdaController::class, 'status']);
+    Route::group([
+        'prefix' => 'bank',
+        'as' => 'bank.',
+    ], function () {
+        Route::get('ada/{id}', [App\Http\Controllers\Bank\AdaController::class, 'create'])->name('ada.create');
+        Route::post('ada', [App\Http\Controllers\Bank\AdaController::class, 'store'])->name('ada.store');
+        Route::get('batch', [App\Http\Controllers\Bank\AdaController::class, 'status']);
 
-    Route::post('donor/search', [App\Http\Controllers\Bank\DonorController::class, 'search'])
-        ->name('donor.search');
+        Route::post('donor/search', [App\Http\Controllers\Bank\DonorController::class, 'search'])
+            ->name('donor.search');
 
-    Route::get('donation/search', function () {
-        return view('bank.donation.search');
-    })->name('donations.search');
-    Route::post('donation/search', [App\Http\Controllers\Bank\DonationController::class, 'search'])
-        ->name('donation.found');
-    Route::resource('donors', App\Http\Controllers\Bank\DonorController::class);
-    Route::resource('donations', App\Http\Controllers\Bank\DonationController::class);
-    Route::resource('demands', 'App\Http\Controllers\Bank\DemandController');
-    Route::resource('inventories', App\Http\Controllers\Bank\InventoryController::class);
-    Route::resource('managers', App\Http\Controllers\Bank\ManagerController::class);
-    Route::resource('ada', App\Http\Controllers\Bank\AdaController::class);
-    Route::resource('camps', App\Http\Controllers\Bank\CampController::class);
-    Route::resource('camps.donations', App\Http\Controllers\Bank\CampDonationController::class);
-
-
-    // Route::resource('processes', App\Http\Controllers\Bank\ProcessController::class);
-    Route::get('processes', [App\Http\Controllers\Bank\ProcessController::class, 'index'])->name('processes.index');
-    Route::post('processes', [App\Http\Controllers\Bank\ProcessController::class, 'store'])->name('processes.store');
-    Route::put('processes/{donation}', [App\Http\Controllers\Bank\ProcessController::class, 'update'])->name('processes.update');
+        Route::get('donation/search', function () {
+            return view('bank.donation.search');
+        })->name('donations.search');
+        Route::post('donation/search', [App\Http\Controllers\Bank\DonationController::class, 'search'])
+            ->name('donation.found');
+        Route::resource('donors', App\Http\Controllers\Bank\DonorController::class);
+        Route::resource('donations', App\Http\Controllers\Bank\DonationController::class);
+        Route::resource('demands', 'App\Http\Controllers\Bank\DemandController');
+        Route::resource('inventories', App\Http\Controllers\Bank\InventoryController::class);
+        Route::resource('managers', App\Http\Controllers\Bank\ManagerController::class);
+        Route::resource('ada', App\Http\Controllers\Bank\AdaController::class);
+        Route::resource('camps', App\Http\Controllers\Bank\CampController::class);
+        Route::resource('camps.donations', App\Http\Controllers\Bank\CampDonationController::class);
 
 
-    Route::group([], function () {
+        // Route::resource('processes', App\Http\Controllers\Bank\ProcessController::class);
+        Route::get('processes', [App\Http\Controllers\Bank\ProcessController::class, 'index'])->name('processes.index');
+        Route::post('processes', [App\Http\Controllers\Bank\ProcessController::class, 'store'])->name('processes.store');
+        Route::put('processes/{donation}', [App\Http\Controllers\Bank\ProcessController::class, 'update'])->name('processes.update');
+
+
+        Route::group([], function () {
+        });
     });
 
-    // Route::prefix('c')->group(function () {
-    //     Route::resource('/donation', 'App\Http\Controllers\Manager\CampDonationController', ['as' => 'camp'])
-    //         ->names([
-    //             // 'index' => 'camp.donation.index',
-    //             // 'create' => 'camp.donation.create',
-    //             // 'store' => 'camp.donation.store',
-    //             // 'show' => 'camp.donation.show',
-    //             // 'update' => 'camp.donation.update',
-    //         ])
-    //         ->parameters([
-    //             'donation' => 'donor'
-    //         ]);
-    // });
+    Route::group([
+        'prefix' => 'donor',
+        'as' => 'donor.',
+    ], function () {
+        Route::resource('donations', App\Http\Controllers\Donor\DonationController::class);
+    });
+
+    Route::group([
+        'prefix' => 'admin',
+        'as' => 'admin.',
+    ], function () {
+        Route::resource(
+            'banks',
+            'App\Http\Controllers\Admin\BankController',
+            // ['as' => 'admin']
+        );
+    });
 });
 
-Route::group([
-    'prefix' => 'donor',
-    'as' => 'donor.',
-    'middleware' => 'auth'
-], function () {
-    Route::resource('donations', App\Http\Controllers\Donor\DonationController::class);
-});
 
-Route::group([
-    'prefix' => 'admin',
-    'as' => 'admin.',
-    'middleware' => 'auth'
-], function () {
-    Route::resource(
-        'banks',
-        'App\Http\Controllers\Admin\BankController',
-        // ['as' => 'admin']
-    );
-});
+
 
 
 
