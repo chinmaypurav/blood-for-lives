3197855d7e71a4ae03da740c8c93849d809808f5
Chinmay Purav <chinmay.purav@focalworks.in>
2021-10-23 23:55:41 +0530

    wip

--- resources/views/bank/camp/index.blade.php
+++ resources/views/bank/camp/index.blade.php
@@ -17,9 +17,11 @@
 
                     <div class="mt-5 flex lg:mt-0 lg:ml-4">
                         <span class="hidden sm:block">
-                        <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            <a href="{{route('bank.camps.create')}}">Create</a>
-                        </button>
+                            <a href="{{route('bank.camps.create')}}">
+                                <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Create
+                                </button>
+                            </a>
                         </span>
                     </div>
 
